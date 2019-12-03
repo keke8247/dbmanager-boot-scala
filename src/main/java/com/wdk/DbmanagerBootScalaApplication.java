999eee5d875dc8cf2e85c24c867cad8b6453486e
wdk <wdk@erongdu.com>
2019-12-03 17:51:24 +0800

    spring boot + data + scala +java做数据库管理

---
+++ src/main/java/com/wdk/DbmanagerBootScalaApplication.java
@@ -0,0 +1,13 @@
+package com.wdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmanagerBootScalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmanagerBootScalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wdk/DbmanagerBootScalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmanagerBootScalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
